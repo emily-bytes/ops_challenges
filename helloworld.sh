16c3ad84cb94562386e2ec1c336e60c23ac676db
Emily Lee <87784443+emily-bytes@users.noreply.github.com>
2022-07-26 23:02:58 -0700

    update helloworld.sh

---
+++ helloworld.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author: Emily Lee
+# Date of latest revision: July 26, 2022
+# Purpose: Print a string to the terminal
+
+# Main
+var="Hello, World!"
+
+#print var
+echo "$var" 
+
+# End
