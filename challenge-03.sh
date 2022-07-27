fc6a276796e419122498fc959c5847db44c14b36
emily <emilit@emilit>
2022-07-27 22:07:35 +0000

    updated helloworld.sh

---
+++ challenge-03.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 03 Ops Challenge Solution
+# Author: Emily Lee
+# Date of latest revision: July 27, 2022
+# Purpose: Write a script that prints out the login history of users
+# via use of a function
+
+# Main
+function print_login_history {
+    var=$(last -i)
+    echo $var
+}
+
+# This calls print_login_history function
+print_login_history
+
+# End
+
---
+++ helloworld.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author: Emilly Lee
+# Date of latest revision: July 26, 2022
+# Purpose: Print a string to the terminal
+
+
+# Main
+var="Hello, World!"
+
+#print var 
+echo "$var" 
+
+# End
