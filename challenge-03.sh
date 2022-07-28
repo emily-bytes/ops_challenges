b101b275739e6608307cba5d697e8d897ec8e262
emily <emilit@emilit>
2022-07-28 01:06:25 +0000

    updating documentation

--- challenge-03.sh
+++ challenge-03.sh
@@ -3,10 +3,12 @@
 # Script: Ops 201 Class 03 Ops Challenge Solution
 # Author: Emily Lee
 # Date of latest revision: July 27, 2022
-# Purpose: Write a script that prints out the login history of users
+# Purpose: This script prints out the login history of users
 # via use of a function
 
 # Main
+# This function prints the log in history of users by
+# assigning the command to var and executing command
 function print_login_history {
     var=$(last -i)
     echo "$var"
