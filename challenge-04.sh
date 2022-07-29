b3c02d6f33bcbfe1cdfdfd38dce82a34fcf94511
emily <emilit@emilit>
2022-07-29 05:54:56 +0000

    added script to file

---
+++ challenge-04.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 04 Ops Challenge Solution
+# Author: Emily Lee
+# Date of latest revision: July 28, 2022
+# Purpose: This script creates directories through the use of an array
+
+# Main
+
+filename="test.txt"
+
+# Create four directories 
+# mkdir {"dir1", "dir2", "dir3", "dir4"}
+mkdir {dir1,dir2,dir3,dir4}
+
+# Declare array to hold four directories 
+DIRECTORY_ARRAY=("dir1" "dir2" "dir3" "dir4") 
+
+# Create a new .txt file within each directory by 
+# referencing the directory with array indices, 
+# not the literal directory path
+
+# for ((i=0; i<${array_length}; i++));
+# do 
+#    cd 
+
+for i in ${DIRECTORY_ARRAY[@]}
+do 
+    cd ${DIRECTORY_ARRAY[$i]}
+    touch $filename
+    cd ..      
+done
+
+# Test and validate 
+ 
