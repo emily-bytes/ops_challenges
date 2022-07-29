faee0a2d288fc493e812de1edb946cff2be01f83
emily <emilit@emilit>
2022-07-29 20:24:34 +0000

    changed string text

--- helloworld.sh
+++ helloworld.sh
@@ -1,26 +1,14 @@
 #!/bin/bash
 
 # Script: Ops 201 Class 02 Ops Challenge Solution
-<<<<<<< HEAD
 # Author: Emilly Lee
 # Date of latest revision: July 26, 2022
 # Purpose: Print a string to the terminal
 
-
 # Main
-var="Hello, World!"
+var="I am working perfectly!"
 
 #print var 
-=======
-# Author: Emily Lee
-# Date of latest revision: July 26, 2022
-# Purpose: Print a string to the terminal
-
-# Main
-var="Hello, World!"
-
-#print var
->>>>>>> 16c3ad84cb94562386e2ec1c336e60c23ac676db
 echo "$var" 
 
 # End
