dbc5281df60f24923fa801d177b861cf915cc1dc
emily <emilit@emilit>
2022-08-01 21:36:47 +0000

    added loop

--- challenge-06.sh
+++ challenge-06.sh
@@ -5,6 +5,41 @@
 # Date of latest revision: August 1, 2022
 # Purpose: Use a loop in the script 
 
+# This script detects if a file or directory exists,
+# then creates it if it does not exist 
+
+# must use at least one array, one loop, and one
+# conditional 
 # Main
 
+# Declare filename 
+# FILE=/home/emilit/ops_challenges/challenge-06.sh
+
+# Declare array 
+FILE_ARRAY=(challenge-06.sh challenge-07.sh challenge-08.sh challenge-09.sh challenge-10.sh) 
+
+for i in ${FILE_ARRAY[@]}
+do 
+    if [ test -f "$i"] ; then   
+        echo "$i exists." 
+    else 
+        echo "$i does not exist" && touch touch $i
+    fi 
+done 
+
 # End
+
+#menu=y
+#until [ $menu = "n" ]; do 
+#   echo Enter a PID to be terminated: 
+#   read PID
+#   kill -15 "$PID"
+
+#   echo Would you like to kill another process? yes = y/no = n
+#   read menu 
+#done 
+
+#for i in ${DIRECTORY_ARRAY[@]}
+#do 
+#    touch ./$i/$filename    # relative path 
+# done
