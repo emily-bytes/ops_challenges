3f3870b503526dd300b736cc97be6ac9ecd0e666
emily <emilit@emilit>
2022-08-02 23:01:32 +0000

    included template

---
+++ challenge-07.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 06 Ops Challenge Solution
+# Author: Emily Lee
+# Date of latest revision: August 1, 2022
+# Purpose: Use grep to remove irrelevant information from the
+# lshw output 
+
+# This script outputs a report about current system CPU 
+# and RAM performance of a Ubuntu computer. 
+# Main
+
+# Welcome message
+
+# Uses lshw to display system information to include: CPU, RAM
+# Display adapter, and Network Adapter. 
+sudo lshw 
+
+
