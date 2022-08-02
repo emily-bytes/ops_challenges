1895f7b18b0b82ed8ee19eeedbbb7d91b7a8a863
emily <emilit@emilit>
2022-08-02 15:21:03 +0000

    added until loop to repeat script

--- challenge-06.sh
+++ challenge-06.sh
@@ -13,24 +13,30 @@
 # Welcome message
 echo "This script detects if a file exists and then creates it if it does not exist"
 
-# Prompt user for filename(s)
-echo "Enter filename(s), separated by space: " 
-read -a filename     # read user input into array called "filename"
+menu=y
+until [ $menu = "n" ]; do 
+    # Prompt user for filename(s)
+    echo "Enter filename(s), separated by space: " 
+    read -a filename     # read user input into array called "filename"
 
-echo $'\nYou have entered '${#filename[@]}' file(s).' 
+    echo $'\nYou have entered '${#filename[@]}' file(s).' 
 
-# For loop 
-for i in ${filename[@]}
-do 
-    # Conditional statment; if the file in the array exists
-    # print "it exists"
-    if [[ -f /home/emilit/ops_challenges/$i ]]; then 
-        echo "$i exists." 
-    else 
-        # else,  state file does not exist and create it 
-        echo "$i does not exist" && touch /home/emilit/ops_challenges/$i
-        echo "$i has been created"
-    fi 
+    # For loop 
+    for i in ${filename[@]}
+    do 
+        # Conditional statment; if the file in the array exists
+        # print "it exists"
+        if [[ -f /home/emilit/ops_challenges/$i ]]; then 
+            echo "$i exists." 
+        else 
+            # else,  state file does not exist and create it 
+            echo "$i does not exist" && touch /home/emilit/ops_challenges/$i
+            echo "$i has been created"
+        fi 
+    done 
+
+    echo Would you like to run this script again? yes = y/no = n
+    read menu 
 done 
 
 # Goodbye message
