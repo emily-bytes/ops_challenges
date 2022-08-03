8c0d5c2aca0e81a488eb569af8e18ca67ecddd16
emily <emilit@emilit>
2022-08-03 16:26:51 +0000

    created template

---
+++ challenge-08.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 08 Ops Challenge Solution
+# Author: Emily Lee
+# Date of latest revision: August 3, 2022
+# Purpose: 
+
+# This script 
+# Main
+
+# Initialize variables 
+
+# End
