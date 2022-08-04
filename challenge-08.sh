751a6c29d2a6c9d8767e4dfef249b25db8bf3d94
emily <emilit@emilit>
2022-08-04 01:52:55 +0000

    windows bash script to create backup files

--- challenge-08.sh
+++ challenge-08.sh
@@ -3,11 +3,14 @@
 # Script: Ops 201 Class 08 Ops Challenge Solution
 # Author: Emily Lee
 # Date of latest revision: August 3, 2022
-# Purpose: 
-
-# This script 
+# Purpose: This script automatically creates backup
+# files for Jorge 
+ 
 # Main
 
-# Initialize variables 
+@echo off
 
+ROBOCOPY "C:\Users\j.thompson\Desktop\My Project Files" "E:\Jorge'sBackups" /LOG:backup.txt
+
+pause
 # End
