e403fc2c5920226d8c5d4e16174a1f09a1de0707
emily <emilit@emilit>
2022-08-09 00:05:18 +0000

    completed challenge 11

---
+++ challenge-11.ps1
@@ -0,0 +1,26 @@
+# Script: Ops 201 Class 11 Ops Challenge Solution
+# Author: Emily Lee
+# Date of latest revision: August 8, 2022
+# Purpose: This script contains a collection of commands that automate Windows 
+# system configurations as much as possible, to save yourself precious time. 
+ 
+# Main
+# Enable File and Printer Sharing
+Set-NetFirewallRule -DisplayGroup "File And Printer Sharing" -Enabled True
+
+# Allow ICMP traffic
+netsh advfirewall firewall add rule name="Allow incoming ping requests IPv4" dir=in action=allow protocol=icmpv4
+
+# Enable remote management 
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+
+# Remove bloatware
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+
+# Enable Hyper-V
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+
+# Disable SMBv1, an insecure protocol
+Set-SmbServerConfiguration -EnableSMB1Protocol $false -Force
+
+# End
