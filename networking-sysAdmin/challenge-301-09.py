f4f1aaa8f353619c3b0862169e90c1aec7dd83a3
emily <emilit@emilit>
2022-09-09 16:36:09 +0000

    documentation

--- networking-sysAdmin/challenge-301-09.py
+++ networking-sysAdmin/challenge-301-09.py
@@ -1,16 +1,21 @@
 #!usr/bin/python 3
 
+# Script: Ops 301 Class 03 Ops Challenge Solution
+# Author: Emily Lee
+# Date of latest revision: September 9, 2022
+# Purpose: This Python script utilizes a list 
+
 # Assign to a variable a list of ten string elements
 coding_languages = ["Java", "C++", "C" , "C#", "JavaScript" , "Python" , "R", "Kotlin", "Go", "Ruby"]
 print("List of coding languages: ")
 print(coding_languages)
 
 # Print the fourth element of the list
-print("\Print the fourth element of the list: ")
+print("Print the fourth element of the list: ")
 print(coding_languages[3])
 
 # Print the sixth through tenth element of the list
-print("\Print the sixth through tenth element of the list: ")
+print("Print the sixth through tenth element of the list: ")
 print(coding_languages[6:11])
 
 # Change the value of the seventh element to "onion"
