ca0977017c7e54cd5d61bd8f19e862faa7425cc8
emily <emilit@emilit>
2022-09-09 17:33:29 +0000

    print on next line

--- networking-sysAdmin/challenge-301-09.py
+++ networking-sysAdmin/challenge-301-09.py
@@ -11,15 +11,15 @@ print("List of coding languages: ")
 print(coding_languages)
 
 # Print the fourth element of the list
-print("Print the fourth element of the list: ")
+print("\nPrint the fourth element of the list: ")
 print(coding_languages[3])
 
 # Print the sixth through tenth element of the list
-print("Print the sixth through tenth element of the list: ")
+print("\nPrint the sixth through tenth element of the list: ")
 print(coding_languages[5:11])
 
 # Change the value of the seventh element to "onion"
-print("Change the value of the seventh element to 'onion'")
+print("\nChange the value of the seventh element to 'onion'")
 coding_languages[6] = "onion"
 print(coding_languages)
 
