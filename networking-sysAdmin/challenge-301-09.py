08e6c44f1088a329f3c2b26b04a2a36882173e1c
emily <emilit@emilit>
2022-09-09 16:34:36 +0000

    first draft of challenge 08 complete

---
+++ networking-sysAdmin/challenge-301-09.py
@@ -0,0 +1,20 @@
+#!usr/bin/python 3
+
+# Assign to a variable a list of ten string elements
+coding_languages = ["Java", "C++", "C" , "C#", "JavaScript" , "Python" , "R", "Kotlin", "Go", "Ruby"]
+print("List of coding languages: ")
+print(coding_languages)
+
+# Print the fourth element of the list
+print("\Print the fourth element of the list: ")
+print(coding_languages[3])
+
+# Print the sixth through tenth element of the list
+print("\Print the sixth through tenth element of the list: ")
+print(coding_languages[6:11])
+
+# Change the value of the seventh element to "onion"
+coding_languages[6] = "onion"
+print(coding_languages)
+
+# End 
