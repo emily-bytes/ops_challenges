aa997f789f8f56f2b53274f358d56a2d94f7e72b
emily <emilit@emilit>
2022-09-13 17:08:10 +0000

    template

--- networking-sysAdmin/challenge-301-11.py
+++ networking-sysAdmin/challenge-301-11.py
@@ -3,13 +3,15 @@
 # Script: Ops 301 Class 11 Ops Challenge Solution
 # Author: Emily Lee
 # Date of latest revision: September 13, 2022
-# Purpose: This Python script creates if statements using
+# Purpose: This Python script creates a new .txt file,
+# appends three lines, prints to the screen the first
+# line, then deletes the .txt file
 
-# open file and write 
+# create new .txt file
 file = open("demofile.txt", "w")
 
 # open a file that exists
-file = open("testfile.txt")
+file = open("demofile.txt")
 
 # open a file and read from it 
 file = open("testfile.txt", "r")
@@ -17,4 +19,7 @@ print(file.read())
 
 # return the first five characters of a file
 file = open("testfile.txt", "r")
-print(file.read())
+print(file.read(5))
+
+# closes a file
+file.close()
