782ce5264a78d4dea0032cf0693112961bff4d1e
emily <emilit@emilit>
2022-09-13 19:48:43 +0000

    first draft of ops challenge 11 done

--- networking-sysAdmin/challenge-301-11.py
+++ networking-sysAdmin/challenge-301-11.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import os 
+
 # Script: Ops 301 Class 11 Ops Challenge Solution
 # Author: Emily Lee
 # Date of latest revision: September 13, 2022
@@ -7,19 +9,18 @@
 # appends three lines, prints to the screen the first
 # line, then deletes the .txt file
 
-# create new .txt file
-file = open("demofile.txt", "w")
-
-# open a file that exists
-file = open("demofile.txt")
-
-# open a file and read from it 
-file = open("testfile.txt", "r")
-print(file.read())
-
-# return the first five characters of a file
-file = open("testfile.txt", "r")
-print(file.read(5))
-
-# closes a file
+# create a new .txt file and append three lines 
+file = open("testfile.txt", "w")
+line = ["I love Code Fellows!\n", "I love coding\n", "Only one week left of 301\n"]
+file.writelines(line)
 file.close()
+
+# print the first line to screen 
+file = open("testfile.txt", "r")
+print(file.read(21))
+
+# delete the .txt file 
+if os.path.exists("testfile.txt"):
+  os.remove("testfile.txt")
+else:
+  print("The file does not exist.")
